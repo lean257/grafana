dc7918b4dabbbf3ded3375a275e70543374fcb57
Ying WANG <ying.wang@grafana.com>
2021-07-08 14:01:48 +0200

    probably need to make the functions and generate exportable

--- pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command.go
+++ pkg/cmd/grafana-cli/commands/scuemata_cuetsy_command.go
@@ -5,9 +5,18 @@ import (
 	"os"
 	"path/filepath"
 
+	"cuelang.org/go/cue"
 	"github.com/grafana/grafana/pkg/cmd/grafana-cli/utils"
 	"github.com/grafana/grafana/pkg/schema/load"
 	"github.com/sdboyer/cuetsy/encoder"
+)
+
+type attrTSTarget string
+
+const (
+	tgtType      attrTSTarget = "type"
+	tgtInterface attrTSTarget = "interface"
+	tgtEnum      attrTSTarget = "enum"
 )
 
 func (cmd Command) generateDashboardTypeScripts(c utils.CommandLine) error {
@@ -27,8 +36,10 @@ func generateTypeScriptFromCUE(dest string, p load.BaseLoadPaths) error {
 	}
 
 	for panelName, panelSchema := range panelSchemaMap {
-		got := fmt.Sprintf("%+v", panelSchema.CUE())
-		fmt.Println("<<<<<<<<<<", got)
+		// got := fmt.Sprintf("%+v", panelSchema.CUE())
+		// fmt.Println("<<<<<<<<<<", got)
+		obj, err := prepare(panelSchema.CUE())
+
 		b, err := encoder.Generate(panelSchema.CUE(), encoder.Config{})
 		if err != nil {
 			return err
@@ -36,6 +47,33 @@ func generateTypeScriptFromCUE(dest string, p load.BaseLoadPaths) error {
 		writeTypeScriptFiles(filepath.Join(dest, panelName+".ts"), string(b))
 	}
 	return nil
+}
+
+func prepare(iCUE cue.Value) (cue.Value, error) {
+	iter, err := iCUE.Fields(cue.Definitions(true))
+	if err != nil {
+		return iCUE, err
+	}
+	for iter.Next() {
+		/*
+			It is very difficult to add new Attribute to Value here,
+			so we probably want just to use the underline function of cuetsy
+		*/
+		a := iter.Value().Attribute("cuetsy")
+		tt, found, err := a.Lookup(0, "targetType")
+		targetType := attrTSTarget(tt)
+
+		if a.Err() != nil || !found || err != nil {
+			targetType = tgtInterface
+		}
+		switch targetType {
+
+		}
+		if err != nil {
+			return iCUE, err
+		}
+	}
+	return iCUE, err
 }
 
 func writeTypeScriptFiles(dest string, content string) error {
